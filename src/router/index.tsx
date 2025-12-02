0eb683ea7e5fbec53dc38f6b93510aa42989bf99
mj-fe <mj.fe.developer@gmail.com>
2025-12-02 18:39:14 -0300

    fix: routes

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,11 @@
 import { lazy } from 'react';
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 import AppLayout from '@/layouts/App';
 
 const Dashboard = lazy(() => import('@/pages/dashboard/Dashboard'));
 const Kanban = lazy(() => import('@/pages/kanban/Kanban'));
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
     {
         path: '/',
         element: <AppLayout />,
